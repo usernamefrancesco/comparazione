59ee97b1098caa663d39914de0317b03e7f0e8a4
NomeFittizio <xxx@users.noreply.github.com>
2025-06-18 15:30:55 +0200

    modifica3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,17 @@ import Navbar from "@/components/Navbar";
 import InputMutui from "@/components/InputMutui";
 import InputGpt from "@/components/InputGpt";
 import Disclamair from "@/components/Disclamair";
+import Head from "next/head";
 export default function Home() {
   return (
     <div className="">
+      <Head>
+        <script
+          data-goatcounter="https://francescomutui.goatcounter.com/count"
+          async
+          src="https://gc.zgo.at/count.js"
+        ></script>
+      </Head>
       <Navbar />
       <div>
         <InputMutui />
