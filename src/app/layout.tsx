25951e0c7733d75070178dca970171a1ef52814e
NomeFittizio <xxx@users.noreply.github.com>
2025-06-24 16:05:15 +0200

    modifica17

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,17 @@
-import { MutuoProvider } from "@/Context/MutuoContext";
 import Script from "next/script";
+import "./globals.css";
+import { MutuoProvider } from "@/Context/MutuoContext";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="it">
-      <head>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17262271115"
-          strategy="afterInteractive"
-          async
-        />
-        <Script id="gtag-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-17262271115');
-          `}
-        </Script>
-      </head>
+      <head />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17262271115"></script>
+
+
+
       <body>
         {/* Plausible */}
         <Script
@@ -27,8 +20,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           src="https://plausible.io/js/script.js"
           strategy="afterInteractive"
         />
+
         <MutuoProvider>
           <main>{children}</main>
+          <SpeedInsights />
         </MutuoProvider>
       </body>
     </html>
